9272744efc7473b3f468b31dfd70bb054e0c2b21
Wiwee <grimstoey@gmail.com>
2026-01-11 16:40:36 +0700

    Task2_Setup_CORS

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import eventRoute from "./routes/EventRoute";
+import path from "path";
+import cors, {CorsOptions} from 'cors';
+
+const app = express();
+const webApp = express();
+
+const port = 3000;
+const webPort = 5050;
+
+const corsOptions:CorsOptions = {
+   origin: ['http://localhost:5050'],
+   methods: ['GET','POST','OPTIONS'],
+   allowedHeaders: ['Content-Type','Authorization'],
+
+
+};
+
+
+app.use(express.json());
+webApp.use(express.static(path.join(process.cwd())));
+app.use(cors(corsOptions));
+
+
+app.use("/events", eventRoute);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+webApp.listen(webPort, () => {
+  console.log(`WebApp listening at http://localhost:${webPort}`);
+});
