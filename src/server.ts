f197edc4c5efd4c79a583199dcc26cbc482b7a7f
Wiwee <grimstoey@gmail.com>
2026-01-15 12:26:25 +0700

    Task3-6

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,77 @@
 import express, { Request, Response } from "express";
 import eventRoute from "./routes/EventRoute";
 import path from "path";
-import cors, {CorsOptions} from 'cors';
+import cors, { CorsOptions } from "cors";
+import multer from "multer";
+import { uploadFile } from "./services/UploadFileService";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const app = express();
 const webApp = express();
+const upload = multer({ storage: multer.memoryStorage() });
 
 const port = 3000;
 const webPort = 5050;
 
-const corsOptions:CorsOptions = {
-   origin: ['http://localhost:5050'],
-   methods: ['GET','POST','OPTIONS'],
-   allowedHeaders: ['Content-Type','Authorization'],
-
-
+const corsOptions: CorsOptions = {
+  origin: ["http://localhost:5050"],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
-
 
 app.use(express.json());
 webApp.use(express.static(path.join(process.cwd())));
 app.use(cors(corsOptions));
 
-
 app.use("/events", eventRoute);
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
+});
+
+app.get("/presignedUrl", async (req: Request, res: Response) => {
+  try {
+    const { key } = req.query;
+    if (!key || typeof key !== "string") {
+      return res.status(400).send("File key is required.");
+    }
+    const bucket = process.env.SUPABASE_BACKET_NAME;
+
+    if (!bucket) {
+      return res.status(500).send("Bucket or file path not configured.");
+    }
+
+    const { getPresignedUrl } = await import("./services/UploadFileService");
+    const presignedUrl = await getPresignedUrl(bucket, key, 3600);
+    res.status(200).json({ url: presignedUrl });
+  } catch (error) {
+    console.error("Error generating presigned URL:", error);
+    res.status(500).send("Error generating presigned URL.");
+  }
+});
+
+app.post("/upload", upload.single("file"), async (req: any, res: any) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).send("No file uploaded.");
+    }
+
+    const bucket = process.env.SUPABASE_BACKET_NAME;
+    const filePath = process.env.UPLOAD_DIR;
+
+    if (!bucket || !filePath) {
+      return res.status(500).send("Bucket name or file path not configured.");
+    }
+
+    const fileKey = await uploadFile(bucket, filePath, file);
+
+    res.status(200).send(fileKey);
+  } catch (error) {
+    res.status(500).send("Error uploading file.");
+  }
 });
 
 webApp.listen(webPort, () => {
